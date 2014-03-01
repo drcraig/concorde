6e8d63cb713a7439d7d4775540a2c60488f1d723
Dan Craig <drcraig@alum.mit.edu>
2014-03-01 01:42:47 -0500

    Split out the functions into three subcommands

--- concorde/__init__.py
+++ concorde/__init__.py
@@ -10,7 +10,7 @@ import dateutil.parser
 from jinja2 import Environment, FileSystemLoader
 import PyRSS2Gen
 
-def get_files(paths, extention='.md', recurse=False):
+def get_files(paths, extensions=['.md', '.markdown'], recurse=False):
     files = []
     for path in paths:
         if os.path.isfile(path):
@@ -19,7 +19,9 @@ def get_files(paths, extention='.md', recurse=False):
             files.extend([os.path.join(root, filename) for filename in filenames])
             if not recurse:
                 break
-    return fnmatch.filter(files, '*'+extention)
+    for extention in extensions:
+        files = fnmatch.filter(files, '*'+extention)
+    return files
 
 def parse_markdown_file(md_file):
     md = Markdown(extensions=['extra', 'meta', 'nl2br', 'sane_lists'])
@@ -96,31 +98,43 @@ def generate_feed(md_files, output_extension, feedfile, feed_url, title='', desc
         rss.write_xml(f)
 
 def main():
-    parser = argparse.ArgumentParser(description="Render Markdown files into a blog")
+    base_parser = argparse.ArgumentParser(add_help=False)
+    base_parser.add_argument('-t', '--template', default='', help='Template file to use when rendering')
+    base_parser.add_argument('-r', '--recurse', action='store_true', default=False, 
+                             help='Recurse through a directory')
+    base_parser.add_argument('--output-extension', help='File extension of the rendered output files')
+    base_parser.add_argument('paths', metavar='path', nargs="+", help='Markdown file or directory of Markdown files')
 
-    parser.add_argument('-e', '--extension', help='File extension', default='.md')
-    parser.add_argument('-t', '--template', default='', help='Template file to use when rendering')
-    parser.add_argument('-r', '--recurse', action='store_true', default=False, 
-                        help='Recurse through a directory')
-    parser.add_argument('--output-extension', help='File extension of the rendered output files')
-    parser.add_argument('-i', '--index', help='Create an index file only, not individual files')
-    parser.add_argument('--rss-file', help='Create an RSS feed')
-    parser.add_argument('--rss-title', default='', help='Title of the RSS feed')
-    parser.add_argument('--rss-description', default='', help='Description of the RSS feed')
-    parser.add_argument('--rss-url', default='', help='URL of the RSS feed')
-    parser.add_argument('paths', metavar='path', nargs="+", help='Markdown file or directory of Markdown files')
+    parser = argparse.ArgumentParser(description="Static site generator")
+    subparsers = parser.add_subparsers(help='subcommand help', dest='subparser_name')
+
+    pages_parser = subparsers.add_parser('pages', help='Render Markdown files to pages', parents=[base_parser])
+
+    index_parser = subparsers.add_parser('index', help='Create an index file', parents=[base_parser])
+    index_parser.add_argument('-o', '--output', required=True, help='Write index to file')
+
+    rss_parser = subparsers.add_parser('rss', help='Create an RSS feed', parents=[base_parser])
+    rss_parser.add_argument('-o', '--output', required=True, help='Write feed to file')
+    rss_parser.add_argument('--title', default='', help='Title of the RSS feed')
+    rss_parser.add_argument('--description', default='', help='Description of the RSS feed')
+    rss_parser.add_argument('--url', default='', help='URL of the RSS feed')
+
     args = parser.parse_args()
 
-    markdown_files = get_files(args.paths, extention=args.extension, recurse=args.recurse)
+    markdown_files = get_files(args.paths, extensions=['.md', '.markdown'],
+                               recurse=args.recurse)
     output_extension = args.output_extension or os.path.splitext(args.template)[1]
 
-    if args.index:
-        render_to_index(markdown_files, args.template, args.index, output_extension)
-    else:
+    if args.subparser_name == 'index':
+        render_to_index(markdown_files, args.template, args.output, output_extension)
+        return
+    if args.subparser_name == 'pages':
         render_articles(markdown_files, args.template, output_extension)
-    if args.rss_file:
-        generate_feed(markdown_files, output_extension, args.rss_file, args.rss_url,
-                      title=args.rss_title, description=args.rss_description)
+        return
+    if args.subparser_name == 'rss':
+        generate_feed(markdown_files, output_extension, args.output, args.url,
+                      title=args.title, description=args.description)
+        return
 
 if __name__ == "__main__":
     main()
