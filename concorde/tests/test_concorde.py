14246d7ce453911e6ab5c365b096a55d65bf3227
Dan Craig <drcraig@alum.mit.edu>
2014-03-04 00:17:25 -0500

    Test directory and file searching

--- concorde/tests/test_concorde.py
+++ concorde/tests/test_concorde.py
@@ -6,23 +6,31 @@ import concorde
 CUR_DIR = os.path.dirname(__file__)
 TESTSITE = os.path.join(CUR_DIR, 'testsite')
 
-class TestConcorde(unittest.TestCase):
-    def test_get_source_files_none(self):
+class TestGetSources(unittest.TestCase):
+    def test_none(self):
         files = concorde.get_source_files([TESTSITE])
         self.assertItemsEqual([], files)
 
-    def test_get_source_files_recursive(self):
+    def test_recursive(self):
         files = concorde.get_source_files([os.path.join(TESTSITE, 'site')], recurse=True)
         expected = ['site/page.md', 'site/another-page.markdown', 
                     'site/blog/post-1.md', 'site/blog/post-2.md']
         self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
         
-    def test_get_source_files_not_recursive(self):
+    def test_not_recursive(self):
         files = concorde.get_source_files([os.path.join(TESTSITE, 'site')], recurse=False)
         expected = ['site/page.md', 'site/another-page.markdown']
         self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
 
         files = concorde.get_source_files([os.path.join(TESTSITE, 'site/blog')], recurse=False)
         expected = ['site/blog/post-1.md', 'site/blog/post-2.md']
+        self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
+
+    def test_mix_dirs_files(self):
+        sources = ['site/page.md', 'site/blog']
+        files = concorde.get_source_files([os.path.join(TESTSITE, f) for f in sources],
+                                          recurse=True)
+        expected = ['site/page.md',
+                    'site/blog/post-1.md', 'site/blog/post-2.md']
         self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
 
