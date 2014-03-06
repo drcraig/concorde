73bb9edb10fbcabe077548a7801f10781830f45f
Dan Craig <drcraig@alum.mit.edu>
2014-03-06 01:56:28 -0500

    Clean up the paths now that I know how

--- concorde/tests/test_concorde.py
+++ concorde/tests/test_concorde.py
@@ -12,32 +12,29 @@ CUR_DIR = os.path.dirname(__file__)
 TESTSITE = os.path.join(CUR_DIR, 'testsite')
 
 class TestGetSources(unittest.TestCase):
+    def setUp(self):
+        os.chdir(TESTSITE)
+
     def test_none(self):
-        files = concorde.get_source_files([TESTSITE])
+        files = concorde.get_source_files(['.'])
         self.assertItemsEqual([], files)
 
     def test_recursive(self):
-        files = concorde.get_source_files([os.path.join(TESTSITE, 'site')], recurse=True)
+        files = concorde.get_source_files(['site'], recurse=True)
         expected = ['site/page.md', 'site/another-page.markdown', 
                     'site/blog/post-1.md', 'site/blog/post-2.md']
-        self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
+        self.assertItemsEqual(expected, files)
         
     def test_not_recursive(self):
-        files = concorde.get_source_files([os.path.join(TESTSITE, 'site')], recurse=False)
-        expected = ['site/page.md', 'site/another-page.markdown']
-        self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
+        files = concorde.get_source_files(['site'], recurse=False)
+        self.assertItemsEqual(['site/page.md', 'site/another-page.markdown'], files)
 
-        files = concorde.get_source_files([os.path.join(TESTSITE, 'site/blog')], recurse=False)
-        expected = ['site/blog/post-1.md', 'site/blog/post-2.md']
-        self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
+        files = concorde.get_source_files(['site/blog'], recurse=False)
+        self.assertItemsEqual(['site/blog/post-1.md', 'site/blog/post-2.md'], files)
 
     def test_mix_dirs_files(self):
-        sources = ['site/page.md', 'site/blog']
-        files = concorde.get_source_files([os.path.join(TESTSITE, f) for f in sources],
-                                          recurse=True)
-        expected = ['site/page.md',
-                    'site/blog/post-1.md', 'site/blog/post-2.md']
-        self.assertItemsEqual([os.path.join(TESTSITE, f) for f in expected], files)
+        files = concorde.get_source_files(['site/page.md', 'site/blog'], recurse=True)
+        self.assertItemsEqual(['site/page.md', 'site/blog/post-1.md', 'site/blog/post-2.md'], files)
 
 FULL_METADATA = '''
 Title: The Title
